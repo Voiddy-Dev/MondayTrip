94f7c7f48b5a57b22aa242e6570860fb30d2ff97
Voiddy-Dev <marinduroyon@gmail.com>
2023-09-23 15:58:58 -0700

    test

--- mondaytrip/src/app/page.tsx
+++ mondaytrip/src/app/page.tsx
@@ -1,33 +1,120 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+"use client";
+import { Suspense } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { ChainId } from "@biconomy/core-types";
+import SocialLogin from "@biconomy/web3-auth";
+import SmartAccount from "@biconomy/smart-account";
+
+
+const MondayTripSocialLogin = () => {
+  const [provider, setProvider] = useState<any>();
+  const [account, setAccount] = useState<string>();
+  const [smartAccount, setSmartAccount] = useState<SmartAccount | null>(null);
+  const [scwAddress, setScwAddress] = useState("");
+  const [scwLoading, setScwLoading] = useState(false);
+  const [socialLoginSDK, setSocialLoginSDK] = useState<SocialLogin | null>(
+    null
+  );
+
+  const connectWeb3 = useCallback(async () => {
+    if (typeof window === "undefined") return;
+    console.log("socialLoginSDK", socialLoginSDK);
+    if (socialLoginSDK?.provider) {
+      const web3Provider = new ethers.providers.Web3Provider(
+        socialLoginSDK.provider
+      );
+      setProvider(web3Provider);
+      const accounts = await web3Provider.listAccounts();
+      setAccount(accounts[0]);
+      return;
+    }
+    if (socialLoginSDK) {
+      socialLoginSDK.showWallet();
+      return socialLoginSDK;
+    }
+    const sdk = new SocialLogin();
+    await sdk.init({
+      chainId: ethers.utils.hexValue(80001),
+    });
+    setSocialLoginSDK(sdk);
+    sdk.showWallet();
+    return socialLoginSDK;
+  }, [socialLoginSDK]);
+
+  useEffect(() => {
+    console.log("hidelwallet");
+    if (socialLoginSDK && socialLoginSDK.provider) {
+      socialLoginSDK.hideWallet();
+    }
+    console.log("socialLoginSDK", socialLoginSDK);
+  }, [account, socialLoginSDK]);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      if (account) {
+        clearInterval(interval);
+      }
+      if (socialLoginSDK?.provider && !account) {
+        connectWeb3();
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [account, connectWeb3, socialLoginSDK]);
+
+  const disconnectWeb3 = async () => {
+    if (!socialLoginSDK || !socialLoginSDK.web3auth) {
+      console.error("Web3Modal not initialized.");
+      return;
+    }
+    await socialLoginSDK.logout();
+    socialLoginSDK.hideWallet();
+    setProvider(undefined);
+    setAccount(undefined);
+    setScwAddress("");
+  };
+
+  useEffect(() => {
+    async function setupSmartAccount() {
+      setScwAddress("");
+      setScwLoading(true);
+      const smartAccount = new SmartAccount(provider, {
+        activeNetworkId: ChainId.GOERLI,
+        supportedNetworksIds: [ChainId.GOERLI],
+      });
+      await smartAccount.init();
+      const context = smartAccount.getSmartAccountContext();
+      setScwAddress(context.baseWallet.getAddress());
+      setSmartAccount(smartAccount);
+      setScwLoading(false);
+    }
+    if (!!provider && !!account) {
+      console.log("Provider...", provider);
+      console.log("Account...", account);
+      setupSmartAccount();
+      // console.log("Provider...", provider);
+    }
+  }, [account, provider]);
+
+  return (
+    <div>
+      <h1>Authentication</h1>
+      <button onClick={!account ? connectWeb3 : disconnectWeb3}>
+        {!account ? "Connect Wallet" : "Disconnect Wallet"}
+      </button>
+    </div>
+  );
+}
 
 export default function Home() {
+
   return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  )
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <MondayTripSocialLogin />
+      </Suspense>
+    </div>
+  );
 }
