5203fbfee1dcfba447f6010bdfe963182d5b6481
Voiddy-Dev <marinduroyon@gmail.com>
2023-09-23 18:16:49 -0700

    setting up a header

--- mondaytrip/src/app/page.tsx
+++ mondaytrip/src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <h1>Home</h1>
     </div>
   )
 }
---
+++ mondaytrip/src/components/header.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button"
+
+export default function Header() {
+    return (
+        <div className="flex items-center justify-between py-4">
+            <div className="flex items-center">
+                <img src="/next.svg" alt="Logo" className="w-10 h-10" />
+                <h1 className="ml-2 text-2xl font-bold">MondayTrip</h1>
+            </div>
+            <div className="flex items-center">
+                <a href="#" className="mr-4 text-sm font-medium text-gray-700 hover:text-gray-900">Home</a>
+                <a href="/explore" className="mr-4 text-sm font-medium text-gray-700 hover:text-gray-900">Explore</a>
+                <a href="/trips" className="mr-4 text-sm font-medium text-gray-700 hover:text-gray-900">My Trips</a>
+            </div>
+            <div className="flex items-center">
+                <Button>Connect</Button>
+            </div>
+        </div>
+    )
+}
